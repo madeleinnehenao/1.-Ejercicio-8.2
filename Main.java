c61cfca5f847fbc3a5c777fdc27f96c99d7e6c2b
madeleinnehenao <124703743+madeleinnehenao@users.noreply.github.com>
2023-06-10 15:17:41 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+package notas;
+
+public class Main {
+
+    public static void main(String[] args) {
+        VentanaPrincipal iniciar=new VentanaPrincipal();
+        iniciar.setVisible(true);
+    }
+}
