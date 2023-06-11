d88027be80644a6fe4990dc52461bbca78b66b41
madeleinnehenao <124703743+madeleinnehenao@users.noreply.github.com>
2023-06-11 13:48:54 -0500

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-package notas;
-
-public class Main {
-
-    public static void main(String[] args) {
-        VentanaPrincipal iniciar=new VentanaPrincipal();
-        iniciar.setVisible(true);
-    }
-}
